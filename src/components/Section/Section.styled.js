655054bbef8e3a9a342685785e7013ffcc8efa32
InnaLyksa <in.lyksa@gmail.com>
2023-01-28 21:38:20 +0200

    Add  Notification, Section  components

---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Wraper = styled.section`
+  margin-bottom: 20px;
+  font-size: 24px;
+`;
+
+export const Title = styled.h2`
+  margin-bottom: 1rem;
+  font-size: 2rem;
+`;
