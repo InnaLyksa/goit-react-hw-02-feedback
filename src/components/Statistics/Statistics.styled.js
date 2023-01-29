e8084e22380203f3f73bbcbe886e725b88ceb38d
InnaLyksa <in.lyksa@gmail.com>
2023-01-29 13:18:26 +0200

    Update Statistics.styled

--- src/components/Statistics/Statistics.styled.js
+++ src/components/Statistics/Statistics.styled.js
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
 
 export const StatisticsList = styled.ul`
-  margin: 0 0 10px 0;
+  margin: 0 0 15px 0;
 `;
 
 export const StatisticsItem = styled.li`
   font-size: 20px;
   font-weight: bold;
+  &:not(:last-child) {
+    margin-bottom: 5px;
+  }
   color: ${({ item }) =>
     (item === 'good' && '#218720') ||
     (item === 'neutral' && '#f1e33f') ||
@@ -17,4 +20,7 @@ export const StatisticsResult = styled.p`
   text-decoration: underline;
   font-size: 20px;
   font-weight: bold;
+  &:not(:last-child) {
+    margin-bottom: 8px;
+  }
 `;
