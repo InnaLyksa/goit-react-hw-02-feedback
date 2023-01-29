df4ab5ae28d98afead8b5b67ea1953add7926a9e
InnaLyksa <in.lyksa@gmail.com>
2023-01-29 11:14:42 +0200

    Update Section.styled.js

--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
 export const Wraper = styled.section`
-  margin-bottom: 20px;
   font-size: 24px;
+  &:not(:last-child) {
+    margin-bottom: 20px;
+  }
 `;
 
 export const Title = styled.h2`
-  margin-bottom: 1rem;
-  font-size: 2rem;
+  margin-bottom: 20px;
+  font-size: 32px;
 `;
